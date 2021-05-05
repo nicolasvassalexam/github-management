9ea1f1874565357ece4d2421a2545b8020970ba1
nicolasvassal <78858987+nicolasvassal@users.noreply.github.com>
2021-05-05 10:12:41 +0200

    Update users.tf

--- module/users.tf
+++ module/users.tf
@@ -3,3 +3,8 @@ resource "github_team" "some_team" {
   description = "Some cool team"
   privacy     = "closed"
 }
+resource "github_repository_collaborator" "a_repo_collaborator" {
+  repository = "our-cool-repo"
+  username   = "SomeUser"
+  permission = "admin"
+}
